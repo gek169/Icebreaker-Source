b3cee223691852860d6d464b2503a2d5089faf43
gek <6974902+gordonfreeman424@users.noreply.github.com>
2021-06-05 23:59:52 -0400

    Automatic commit.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #-DDATAPREFIX=\"$(datadir)\" -DHISCOREPREFIX=\"$(highscoredir)\"
-tcc *.c -o bin/icebreaker -lSDL -lSDL_mixer\
+gcc *.c -o bin/icebreaker -lSDL -lSDL_mixer\
  -lmad -logg -lmikmod -I/usr/include/SDL/ -O3\
   -DDATAPREFIX=\"/home/gek/applications_custom/icebreaker\" -DHISCOREPREFIX=\"/home/gek/applications_custom/icebreaker\"
